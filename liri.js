d6f6ec3d142d152e7763111c6fda5194f928fb7e
Jeff Andony <jeff.andony@gmail.com>
2019-06-25 16:17:31 -0600

    added results when user's don't input anything.

--- liri.js
+++ liri.js
@@ -7,11 +7,11 @@ var fs = require("fs");
 
 
 var userCommand = process.argv[2];
+var userInput = process.argv.slice(3).join("+");
 
 function concertThis() {
     // Bands in Town Artist Events API goes here:
-    var artist = process.argv.slice(3).join("");
-    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    var queryUrl = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
 
     axios.get(queryUrl).then(function(response) {
         // if (err) return console.log(err);
@@ -26,40 +26,54 @@ function concertThis() {
     });
 }
 
-function spotifyThisSong() {
-    var song = process.argv.slice(3).join("");
+function spotifyThisSong(userInput) {
 
-    spotify.search({ type: 'track', query: song}, function (err, response) {
+    var newCommand = userInput.split("+").join(" ");
+    // console.log(newCommand);
+
+    spotify.search({ type: 'track', query: newCommand}, function (err, response) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log("------------");
-        console.log("Artist: " + response.tracks.items[0].artists[0].name);
-        console.log("Song: " + response.tracks.items[0].name);
-        console.log("URL: " + response.tracks.items[0].external_urls.spotify);
-        console.log("Album: " + response.tracks.items[0].album.name);
-        console.log("------------");
+
+        if ("The Sign") {
+            console.log("------------");
+            console.log("Artist: " + response.tracks.items[5].artists[0].name);
+            console.log("Song: " + response.tracks.items[5].name);
+            console.log("URL: " + response.tracks.items[5].external_urls.spotify);
+            console.log("Album: " + response.tracks.items[5].album.name);
+            console.log("------------");
+        } else if (userInput){
+            console.log("------------");
+            console.log("Artist: " + response.tracks.items[0].artists[0].name);
+            console.log("Song: " + response.tracks.items[0].name);
+            console.log("URL: " + response.tracks.items[0].external_urls.spotify);
+            console.log("Album: " + response.tracks.items[0].album.name);
+            console.log("------------");
+        }
     });
 }
 
-function movieThis() {
-    var movie = process.argv.slice(3).join("+");
-
+function movieThis(userInput) {
     // Then run a request with axios to the OMDB API with the movie specified
-    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
 
     axios.get(queryUrl).then(function(response) {
-    console.log("------------");
-    console.log("Title: " + response.data.Title);
-    console.log("Year: " + response.data.Year);
-    console.log("IMDB Rating: " + response.data.Rated);
-    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
-    console.log("Country: " + response.data.Country);
-    console.log("Language: " + response.data.Language);
-    console.log("Plot: " + response.data.Plot);
-    console.log("Actors: " + response.data.Actors);
-
-    console.log("------------");
+        if (userInput === "Mr. Nobody" || userInput === "Mr.+Nobody") {
+            console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+            console.log("It's on Netflix!");
+        } else {
+            console.log("------------");
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.Rated);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+            console.log("------------");
+        }
     });
 }
 
@@ -70,6 +84,7 @@ function doWhatItSays() {
         }
         var song = data.split(",");
         console.log(song[1]);
+        spotifyThisSong(song[1]);
         
     }
 )};
@@ -79,10 +94,18 @@ switch(userCommand) {
         concertThis();
         break;
     case "spotify-this-song":
-        spotifyThisSong();
+        if (userInput) {
+            spotifyThisSong(userInput);
+        } else {
+            spotifyThisSong("The Sign");
+        }
         break;
     case "movie-this":
-        movieThis();
+        if (userInput) {
+            movieThis(userInput);
+        } else {
+            movieThis("Mr. Nobody")
+        }
         break;
     case "do-what-it-says":
         doWhatItSays();
